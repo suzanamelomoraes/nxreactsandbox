00e552dcd47414d8a7875895e7805d58993432e6
Suzana Melo <suzanamelojor@gmail.com>
2023-11-22 23:54:05 +1300

    nxreactsandbox: include order-list components in the router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 // importing the component from the library
 import { ProductList } from '@nxreactsandbox/modules/products';
+import { OrdersList } from '@nxreactsandbox/modules/orders';
 
 function Home() {
   return <h1>Home</h1>;
@@ -12,6 +13,7 @@ export function App() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/products" element={<ProductList />}></Route>
+      <Route path="/orders" element={<OrdersList />}></Route>
     </Routes>
   );
 }
